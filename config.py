2a1bbb0c4c412f2051576a881be78117a53cfa02
devlopss <jensbens67@gmail.com>
2026-08-29 18:17:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py - Конфигурация бота
 
-BOT_TOKEN ="8412574807:AAGnKVaflcLX5hoX23nr8_GC3z_w3XLvCDI"
+BOT_TOKEN = "8412574807:AAGnKVaflcLX5hoX23nr8_GC3z_w3XLvCDI"
 ADMIN_IDS = [7317419505]
 
 PROXY_FILE = 'proxy.txt'
